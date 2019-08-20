aa49d3402e77df01810aee0763978fb530262ed4
Your Name <you@example.com>
2019-08-20 10:45:35 +0700

    first commit

---
+++ src/praktikum1/DataDiri.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum1;
+
+/**
+ *
+ * @author Asus
+ */
+public class DataDiri {
+    
+public static void main(String[] args) {
+    // membuat variabel
+    String nama, alamat, hobi, motto;
+    int usia;
+    double tinggi;
+    
+    //mengisi variable
+    nama = "Rensi Meila";
+    alamat = "Kepung";
+    hobi = "membaca chat tanpa membalas";
+    motto = "bertemu dengan okke awokawoka";
+    usia = 15;
+    tinggi = 162;
+    
+    //mencetak ke layar isi variabel
+    System.out.println("Nama: " + nama);
+    System.out.println("Alamat: " + alamat);
+    System.out.println("hobi: " + hobi); 
+    System.out.println("motto: " + motto);
+    System.out.println("Usia: " + usia + " tahun");
+    System.out.println("tinggi: " + tinggi + " cm");
+}
